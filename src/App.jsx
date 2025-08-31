ad705a27952b8326df19560edb577258051d33a9
sujitIwale <sujit.iwale5115@gmail.com>
2025-08-31 19:09:48 +0530

    project init

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/Home";
+import NestedCheckboxTree from "./pages/NestedCheckBox/NestedCheckBox";
+import Clock from "./pages/Clock/Clock";
+import FolderTreePage from "./pages/FolderTree/FolderTreePage";
+import InfiniteScroll from "./pages/InfiniteScroll/v1/InfiniteScroll";
+import InfiniteScrollV2 from "./pages/InfiniteScroll/v2/InfiniteScrollV2";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/nested-checkbox" Component={NestedCheckboxTree} />
+        <Route path="/clock" Component={Clock} />
+        <Route path="/folder-tree" Component={FolderTreePage} />
+        <Route path="/infinite-scroll" Component={InfiniteScroll} />
+        <Route path="/infinite-scroll/v2" Component={InfiniteScrollV2} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
